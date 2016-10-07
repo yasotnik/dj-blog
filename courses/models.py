1cbb071c05e4de0d11e8b35094ce84a9fb1dd04e
yasotnik <sotnik.edu@ya.ru>
2016-10-07 19:35:38 +0300

    Created model of course

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    teacher = models.CharField(max_length=50)
+    length = models.IntegerField()
+    field = models.CharField(max_length=150)
+    course_logo = models.CharField(max_length=1000)
+
+
+class Lectures(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    lecture_name = models.CharField(max_length=100)
