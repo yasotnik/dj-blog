d29b3e4e5a1a8f795181ec27947c98f4e9b46929
yasotnik <sotnik.edu@ya.ru>
2016-10-07 23:54:11 +0300

    Added __str__ method for models

--- courses/models.py
+++ courses/models.py
@@ -8,8 +8,14 @@ class Course(models.Model):
     field = models.CharField(max_length=150)
     course_logo = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.name + ", " + self.teacher
+
 
 class Lectures(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     lecture_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.course.name + ", " + self.lecture_name
